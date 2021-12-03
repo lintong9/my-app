75d2cafb9fc0185ef3c45c57cfdeef6c8b2790e8
lintong <18511245965@163.com>
2021-12-03 14:40:14 +0800

    添加新页面

---
+++ src/pages/firstPage/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="userInfo">
+    <img v-if=userInfo.img :src=userInfo.img alt="" />
+    <div class="user-nickName">
+      {{ userInfo.name }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        name: "lintong",
+        age: 30,
+        img: "/static/images/user.png",
+      },
+    };
+  },
+};
+</script>
